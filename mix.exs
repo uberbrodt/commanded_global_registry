5975cb451c883ad9a4ea36eed1b2f8d016610bb8
Chris Brodt <chris@uberbrodt.net>
2019-05-21 19:54:23 -0500

    cleanup

--- mix.exs
+++ mix.exs
@@ -31,8 +31,6 @@ defmodule Commanded.Registration.Global.MixProject do
       {:jason, "~> 1.1"},
       {:local_cluster, "~> 1.0", only: [:test]},
       {:mox, "~> 0.5", only: :test}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
